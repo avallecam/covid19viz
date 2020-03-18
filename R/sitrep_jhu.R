d61dc29db877607b2259870ee962f329909de9b4
avallecam <avallecam@gmail.com>
2020-03-17 21:56:51 -0500

    add purrr summary functions

--- R/sitrep_jhu.R
+++ R/sitrep_jhu.R
@@ -9,11 +9,14 @@
 #' @import readr
 #' @import dplyr
 #' @import tidyr
+#' @import purrr
 #'
 #' @return import and cleaned jhu dataset
 #'
 #' @export jhu_sitrep_import
 #' @export jhu_sitrep_cleandb
+#' @export jhu_sitrep_cummulative
+#' @export jhu_sitrep_cummulative_all_sources
 #'
 #' @examples
 #'
@@ -27,7 +30,12 @@
 #'   filter(country_region=="Peru") %>%
 #'   arrange(desc(dates))
 #'
-
+#' jhu_sitrep_import(source = "confirmed") %>%
+#'   jhu_sitrep_cleandb() %>%
+#'   jhu_sitrep_cummulative()
+#'
+#' jhu_sitrep_cummulative_all_sources(country_region="Peru")
+#'
 jhu_sitrep_import <- function(source) {
   if (source=="confirmed") {
     path_start <- "https://raw.github.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
@@ -63,4 +71,35 @@ jhu_sitrep_cleandb <- function(data) {
     janitor::clean_names() %>%
     mutate(dates=lubridate::mdy(dates)) %>%
     select(source,country_region,province_state,everything())
+}
+
+#' @describeIn jhu_sitrep_import clean jhu dataset
+#' @inheritParams jhu_sitrep_import
+#' @param country_region input country name in english
+
+jhu_sitrep_cummulative <- function(data,country_region="all") {
+  if (country_region!="all") {
+    data_filter <- data %>%
+      filter(country_region=={{country_region}})
+  } else {
+    data_filter <- data
+  }
+  data_filter %>%
+    group_by(country_region) %>%
+    filter(dates==last(dates)) %>%
+    ungroup() %>%
+    summarise(tot_value=sum(value)) %>%
+    pull(tot_value)
+}
+
+#' @describeIn jhu_sitrep_import clean jhu dataset
+#' @inheritParams jhu_sitrep_import
+
+jhu_sitrep_cummulative_all_sources <- function(country_region="all") {
+  expand_grid(country_region={{country_region}},
+              source=c("confirmed","deaths","recovered")) %>%
+    mutate(data=map(.x = source,.f = jhu_sitrep_import)) %>%
+    mutate(data_clean=map(.x = data,.f = jhu_sitrep_cleandb)) %>%
+    mutate(sum_data=pmap_dbl(.l = select(.,data=data_clean,country_region),
+                             .f = jhu_sitrep_cummulative))
 }
